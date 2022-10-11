daa1be0d14ee907701e36bf657d481fcf4c3d6ce
Nathan Spix <56930974+njspix@users.noreply.github.com>
2022-10-11 09:51:13 -0400

    add another parsing mode

--- src/parse_file.jl
+++ src/parse_file.jl
@@ -54,7 +54,7 @@ function parse_rle(rle::String)
 end 
 
 #Note that if we call EPIREAD.chromstart(record) to get the start position of the read, it is 1-based, end-inclusive (*on-base*). Try to match this here. 
-function get_feature_pos_in_read(rle::String; mode = 'M')
+function get_feature_pos_in_read(rle::String; mode::Char)
 	J = Vector{Int64}()
 	V = Vector{Int}()
 	counter = 0
@@ -62,6 +62,9 @@ function get_feature_pos_in_read(rle::String; mode = 'M')
     if mode == 'M'# methylation + SNP
         ignore_base = "FPx_OSQ" # sequencing errors and open/shut don't indicate different epialleles
         feature_base = "MUACGTN"
+    elseif mode == 'S' # SNP only
+        ignore_base = "FPx_MUOSQ" # sequencing errors and open/shut don't indicate different epialleles
+        feature_base = "ACGTN"
     end
 
     try
@@ -95,8 +98,8 @@ function get_feature_pos_in_read(rle::String; mode = 'M')
 	return (J, V, nothing)
 end
 
-function get_feature_pos_absolute(rle::String, read_start::Int64)
-	J, V, error = get_feature_pos_in_read(rle)
+function get_feature_pos_absolute(rle::String, read_start::Int64; mode::Char)
+	J, V, error = get_feature_pos_in_read(rle, mode = mode)
 	return (J.+read_start.-1, V, error) # we have to subtract one, as this is 1-start, fully closed
 end
 
@@ -108,9 +111,9 @@ function update_vecs(master_I::Vector{Int64}, master_J::Vector{Int64}, master_V:
 	return nothing
 end
 
-function analyze_read(read::EPIREAD.Record, master_I::Vector{Int64}, master_J::Vector{Int64}, master_V::Vector{Int})
+function analyze_read(read::EPIREAD.Record, master_I::Vector{Int64}, master_J::Vector{Int64}, master_V::Vector{Int}; mode::Char)
 	# println("analyzing read $(EPIREAD.name(read))/$(EPIREAD.readnum(read)) ($(EPIREAD.chrom(read))): $(EPIREAD.cg_rle(read))")
-	J, V, error = get_feature_pos_absolute(EPIREAD.cg_rle(read), EPIREAD.chromstart(read))
+	J, V, error = get_feature_pos_absolute(EPIREAD.cg_rle(read), EPIREAD.chromstart(read), mode = mode)
     # This method is only used for analyzing single reads, so we should never get an IndelError. If we do get an error, we need to throw it:
     if isa(error, Exception)
         throw(error)
@@ -119,9 +122,9 @@ function analyze_read(read::EPIREAD.Record, master_I::Vector{Int64}, master_J::V
 	return nothing
 end
 
-function analyze_read(rle::String, chromstart::Int64, read_name::String, master_I::Vector{Int64}, master_J::Vector{Int64}, master_V::Vector{Int}, indel_error_reads::Vector{String})
+function analyze_read(rle::String, chromstart::Int64, read_name::String, master_I::Vector{Int64}, master_J::Vector{Int64}, master_V::Vector{Int}, indel_error_reads::Vector{String}; mode::Char)
 	# println("analyzing read $(EPIREAD.name(read))/$(EPIREAD.readnum(read)) ($(EPIREAD.chrom(read))): $(EPIREAD.cg_rle(read))")
-	J, V, error = get_feature_pos_absolute(rle, chromstart)
+	J, V, error = get_feature_pos_absolute(rle, chromstart, mode = mode)
     # this method is used for analyzing merged paired reads, so we might get an IndelError. If we do, add the read name to the IndelError Vector
     if isa(error, Exception)
         push!(indel_error_reads, read_name)
@@ -184,25 +187,25 @@ function merge_rle(read1::EPIREAD.Record, read2::EPIREAD.Record)
 	return (merge_rle(long_rle, short_rle), EPIREAD.chromstart(left_read))
 end
 
-function analyze_record_pair(read1::EPIREAD.Record, read2::EPIREAD.Record, master_I::Vector{Int64}, master_J::Vector{Int64}, master_V::Vector{Int}, indel_error_reads::Vector{String})
+function analyze_record_pair(read1::EPIREAD.Record, read2::EPIREAD.Record, master_I::Vector{Int64}, master_J::Vector{Int64}, master_V::Vector{Int}, indel_error_reads::Vector{String}; mode::Char)
 	if !allsame(EPIREAD.chrom.((read1, read2)))
 		for each in (read1, read2)
-			analyze_read(each, master_I, master_J, master_V)
+			analyze_read(each, master_I, master_J, master_V, mode = mode)
 		end
 	else 
 		if !are_overlapped(read1, read2)
 			for each in (read1, read2)
-				analyze_read(each, master_I, master_J, master_V)
+				analyze_read(each, master_I, master_J, master_V, mode = mode)
 			end
 		else
 			# handle overlapping reads
 			rle, chromstart = merge_rle(read1, read2)
-			analyze_read(rle, chromstart, EPIREAD.name(read1), master_I, master_J, master_V, indel_error_reads)
+			analyze_read(rle, chromstart, EPIREAD.name(read1), master_I, master_J, master_V, indel_error_reads, mode = mode)
 		end 
 	end
 end
 
-function analyze_chr(reader::EPIREAD.Reader, max_isize::Int, leftover_record::EPIREAD.Record)
+function analyze_chr(reader::EPIREAD.Reader, max_isize::Int, leftover_record::EPIREAD.Record; mode::Char)
 
     i = 0 # counter for number of reads analyzed
     I,J,V = Vector{Int64}(), Vector{Int64}(), Vector{Int}() # vectors for sparse matrix
@@ -230,7 +233,7 @@ function analyze_chr(reader::EPIREAD.Reader, max_isize::Int, leftover_record::EP
         if (last_chr ≠ "" && last_chr ≠ current_chr)
             # Clean out the cache
             for each in record_cache
-                analyze_read(each, I, J, V)
+                analyze_read(each, I, J, V, mode = mode)
             end
             println(Base.stderr, "Parsed $i reads in $(last_chr)")
             return (sparse(I,J,V), indel_error_reads, last_chr, record)
@@ -243,14 +246,14 @@ function analyze_chr(reader::EPIREAD.Reader, max_isize::Int, leftover_record::EP
                 # If either of the above is true, get all the un-pair-able reads out of the cache and analyze them:
                 # println("cleaning cache because min_end $leftmost_chromend is more than $max_isize away from $chromstart or because last-seen $(last_chr) is different from current $(EPIREAD.chrom(record)) (read $(name))")
                 for each in clean_record_cache!(record_cache, chromstart - max_isize, EPIREAD.chrom(record))
-                    analyze_read(each, I, J, V)
+                    analyze_read(each, I, J, V, mode = mode)
                 end
             end
             # Ok, any read still in the cache is fair game to pair with our current read. Let's see if we can find a match:
             cached_names = EPIREAD.name.(record_cache)
             if name in cached_names 
                 pair_record = popat!(record_cache, findfirst(isequal(name), cached_names))
-                analyze_record_pair(record, pair_record, I, J, V, indel_error_reads)
+                analyze_record_pair(record, pair_record, I, J, V, indel_error_reads, mode = mode)
             # Ok, we couldn't find a match. Save this read for later (maybe its match just hasn't been read yet!)
             else
                 push!(record_cache, copy(record))
@@ -261,14 +264,14 @@ function analyze_chr(reader::EPIREAD.Reader, max_isize::Int, leftover_record::EP
 
     # Clean out the cache
     for each in record_cache
-        analyze_read(each, I, J, V)
+        analyze_read(each, I, J, V, mode = mode)
     end
     println(Base.stderr, "Completed analyis of $i reads in $(last_chr)")
     return (sparse(I,J,V), indel_error_reads, last_chr, EPIREAD.Record())
 end
 
 
-function parse_infile(filename::String; max_isize = 1000)
+function parse_infile(filename::String; max_isize = 1000, mode::Char)
 
     results = Dict{String, SparseMatrixCSC{Int, Int64}}()
     indel_error_reads = Vector{String}()
@@ -276,7 +279,7 @@ function parse_infile(filename::String; max_isize = 1000)
 
     reader = EPIREAD.Reader(filename)
     while !eof(reader)
-        (sparse_results, new_indel_errors, last_chr, leftover_record) = analyze_chr(reader, max_isize, leftover_record)
+        (sparse_results, new_indel_errors, last_chr, leftover_record) = analyze_chr(reader, max_isize, leftover_record, mode = mode)
         results[last_chr] = sparse_results
         append!(new_indel_errors, indel_error_reads)
     end
@@ -297,40 +300,40 @@ Epiallele parsing and counting functions
 #= We need to set up this struct to allow for quick comparisons between epireads. 
 	If they only differ at 'N' bases, they should be treated as identical =#
 
-    struct Epiallele <: AbstractVector{Int64}
-        x::Vector{Int64}
+struct Epiallele <: AbstractVector{Int64}
+    x::Vector{Int64}
+end
+
+function Base.size(x::Epiallele)
+    return size(x.x)
+end
+
+function Base.getindex(x::Epiallele, i::Int64)
+    return x.x[i]
+end
+
+function Base.:(==)(x::Epiallele, y::Epiallele) 
+    # if the epialleles are different lengths, return false
+    if length(x.x) != length(y.x)
+        return false
     end
-    
-    function Base.size(x::Epiallele)
-        return size(x.x)
-    end
-    
-    function Base.getindex(x::Epiallele, i::Int64)
-        return x.x[i]
-    end
-    
-    function Base.:(==)(x::Epiallele, y::Epiallele) 
-        # if the epialleles are different lengths, return false
-        if length(x.x) != length(y.x)
+    # compare elementwise; if only difference is at N's (78), return true
+    for i in 1:length(x.x)
+        if x.x[i] != y.x[i] && x.x[i] != 78 && y.x[i] != 78
             return false
         end
-        # compare elementwise; if only difference is at N's (78), return true
-        for i in 1:length(x.x)
-            if x.x[i] != y.x[i] && x.x[i] != 78 && y.x[i] != 78
-                return false
-            end
-        end
-        return true
     end
-    
-    function Base.isequal(x::Epiallele, y::Epiallele)
-        return x == y
-    end
-    
-    # Zero initializer for Epiallele
-    function Base.zeros(::Type{Epiallele}, n::Int64)
-        return Epiallele(zeros(Int64, n))
-    end
+    return true
+end
+
+function Base.isequal(x::Epiallele, y::Epiallele)
+    return x == y
+end
+
+# Zero initializer for Epiallele
+function Base.zeros(::Type{Epiallele}, n::Int64)
+    return Epiallele(zeros(Int64, n))
+end
 
 function get_unsparse_nonzero_rows(matrix::SparseMatrixCSC{Int64, Int64})
 	nzrows = unique(matrix.rowval)
@@ -370,38 +373,39 @@ end
 
 function tally_epialles(matrix::SparseMatrixCSC{Int64, Int64}, chr::String; window_size = 4::Int)
     n_bases = size(matrix, 2)
-    println("fixedStep\tchrom=$(chr)\tstart=1\tstep=$(window_size)")
     current_bed_line = (0::Int, 0::Int) # (start, n_alleles)
     for i in 1:window_size:n_bases
-        if i % 10000 == 1
+        if i % 100000 == 1
             println(Base.stderr, "Tallying epialleles on $(chr) at position $(i) of $(n_bases)")
             flush(stdout)
         end
-    
         upper_limit = minimum([i+window_size-1, n_bases])
         a = get_unsparse_nonzero_rows(matrix[:,i:upper_limit])
+
+        n = 0
         if !isempty(a)
             n = count_epialleles(matrix_to_epiallele_vector(a))
-            if n != current_bed_line[2]
-                println("$(chr)\t$(current_bed_line[1])\t$(upper_limit)\t$(n)")
-                current_bed_line = (i-1, n)
-            end
+        end
+
+        if n != current_bed_line[2]
+            println("$(chr)\t$(current_bed_line[1])\t$(i-1)\t$(current_bed_line[2])")
+            current_bed_line = (i-1, n)
         end
     end
 end
 
-function output_stats(results::Dict{String, SparseMatrixCSC{Int64, Int64}})
+function output_stats(results::Dict{String, SparseMatrixCSC{Int64, Int64}}; window_size = 4::Int)
     println("track type=bedGraph")
     chrs = collect(keys(results))
     for chr in sort(chrs)
         println(Base.stderr, "Processing $chr")
-        tally_epialles(results[chr], chr)
+        tally_epialles(results[chr], chr, window_size = window_size)
     end
 end
 
-function process_file(filename::String; max_isize = 1000)
-    results = parse_infile(filename, max_isize = max_isize)
-    output_stats(results)
+function process_file(filename::String; max_isize = 1000, window_size = 4::Int, mode::Char = 'M')
+    results = parse_infile(filename, max_isize = max_isize, mode = mode)
+    output_stats(results, window_size = window_size)
 end
 
-process_file("./testing/test_epiread.bed")
+process_file("./testing/luo_epiread.bed", window_size = 10)
